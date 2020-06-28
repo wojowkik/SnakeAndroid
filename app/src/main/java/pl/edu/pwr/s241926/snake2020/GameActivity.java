6829ec922d5a9cb9df0d7f022597376c5348faba
wojowkik <wojciech.bondyra@gmail.com>
2020-06-28 12:30:56 +0200

    gameActivityLockOrientation

--- app/src/main/java/pl/edu/pwr/s241926/snake2020/GameActivity.java
+++ app/src/main/java/pl/edu/pwr/s241926/snake2020/GameActivity.java
@@ -2,6 +2,7 @@ package pl.edu.pwr.s241926.snake2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class GameActivity extends AppCompatActivity {
@@ -10,5 +11,6 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
     }
 }
